e15930838df3be91f440030bd367fc52aa51434d
Ana Paula Nunes Cerchiari Almeida <aalmeida2014@kellogg.northwestern.edu>
2013-11-03 17:57:11 -0600

    Create and delete pictures
    Create and delete pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,8 +8,20 @@ class PicturesController < ApplicationController
   end
 
   def new
+
   end
 
   def create
+    p = Picture.new
+    p.source = params[:source]
+    p.caption = params[:caption]
+    p.save
+    @picture = p
+  end
+
+  def destroy
+    i = Picture.find(params[:id])
+    i.destroy
+    @picture = i
   end
 end
